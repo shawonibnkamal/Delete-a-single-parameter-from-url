81d074acc68a39e15750c17d89729804fd6cc867
Shawon Kamal <me@shawon.dev>
2020-05-19 16:42:16 -0230

    js function

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Delete-a-single-paramter-from-url
+
+This javascript functions allows to delete a single paramter given the key and value. I wrote this function because the <a href="https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams">URLSearchParams</a> interface deletes all the parameters instead of just 1.
+
+
+```js
+// Given url is
+// https://example.com?foo=1&bar=2&foo=3
+
+deleteParam("foo", 1)
+
+//Output:
+// https://example.com?bar=2&foo=3
+```
---
+++ main.js
@@ -0,0 +1,29 @@
+function deleteParam(key, value) {
+  if (!window.history.pushState) {
+    return;
+  }
+
+  if (!key) {
+    return;
+  }
+
+  //gets the parameters from url and puts it in the list
+  //newParamList except the parameter that is passed in to
+  //be deleted
+  var paramList = window.location.search.substr(1).split("&");
+  let newParamList = [];
+  for (let i = 0; i < paramList.length; i++) {
+    let pair = paramList[i].split("=");
+    if (pair[0] != key || pair[1] != value) {
+      pair = pair.join("=");
+      newParamList.push(pair);
+    }
+  }
+  newParam = "?" + newParamList.join("&");
+  console.log(newParam);
+  newParamObject = new window.URLSearchParams(newParam);
+
+  url.search = newParamObject;
+  url = url.toString();
+  window.history.replaceState({ url: url }, null, url);
+}
